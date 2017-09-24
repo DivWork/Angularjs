6d771d2b61f44ca4fa4eeb0718392be1c3830315
divyesh_ <divyesh@medikoe.com>
2017-09-25 01:36:09 +0530

    setup

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1> Welcome Express </h1>');
+const mongoose = require('mongoose');
+
+const config = require('./config/database');
+
+const path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri,(err)=>{
+	if(err){
+		console.log('Databse not connected',err);
+	}else{
+		console.log('connected to database : ' +config.db);
+	}
 });
 
-app.listen(8080,()=>{
+app.use(express.static(__dirname+'/client/dist/'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+});
+
+app.listen(8010,()=>{
 	console.log("Hello world");
 });
