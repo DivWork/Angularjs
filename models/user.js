0b4a27f5b5e47b53f2487dc84218559c983e67f9
divyesh_ <divyesh@medikoe.com>
2017-09-26 18:05:00 +0530

    authentication.js with proper message and validator

--- models/user.js
+++ models/user.js
@@ -1,12 +1,96 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
 
 var bcyrpt = require('bcrypt-nodejs');
+let emailLengthChecker = (email) => {
+  if(!email){
+    return false;
+  }else{
+    if(email.length < 5 || email.length > 40){
+      return false;
+    }else{
+        return true;
+    }
+  }
+};
 
-var Schema = mongoose.Schema;
+let ValidemailChecker = (email) =>{
+  if(!email){
+    return false;
+  }else{
+    const regExp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+    return regExp.test(email);
+  }
+};
+
+let UserNameLengthChecker = (userName) =>{
+  if(!userName){
+    return false;
+  }else{
+    if(userName.length< 3 || userName.length > 20){
+      return false;
+    }else{
+      return true;
+    }
+  }
+};
+
+let validUsername = (userName) => {
+  if(!userName){
+    return false;
+  }else{
+    const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+    return regExp.test(userName);
+  }
+};
+
+let passwordValidLengthChecker = (password) => {
+  if(!password){
+    return false;
+  }else{
+    if(password.length<8 || password.length > 35){
+      return false;
+    }else{
+      return true;
+    }
+  }
+};
+
+let validPassword = (password) => {
+ if(!password){
+   return false;
+ }else{
+   const regExp = new RegExp(/^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])(?=.*?[\W]).{8,35}$/);
+   return regExp.test(password);
+ }
+};
+
+const usernameValidators = [
+  {
+    validator: UserNameLengthChecker,
+    message: 'user name should be more than 3 characters and should be less than 20'
+  },
+  {
+    validator: validUsername,
+    message: 'User Name should be specific'
+  }
+]
+
+const emailValidators = [
+    {
+      validator: emailLengthChecker, message:'Email must be atleast 5 characters'
+    },
+    {
+      validator: ValidemailChecker, message: 'Valid email check failed'
+    }
+
+]
+
+
 var userSchema = new Schema({
-  email : {type: String, required:true, unique: true, lowercase: true},
-  userName : {type: String, required:true, unique: true, lowercase: true},
+  email : {type: String, required:true, unique: true, lowercase: true, validate: emailValidators},
+  userName : {type: String, required:true, unique: true, lowercase: true, validate: usernameValidators},
   password : {type: String, required: true}
 });
 
--- routes/authentication.js
+++ routes/authentication.js
@@ -9,7 +9,6 @@ module.exports = (router) =>{
     if(!req.body.email){
       res.json({success:false, message: 'provide an email'});
     }else{
-      // res.send(req.body.username);
       if(!req.body.username){
         res.json({success:false, message: 'provide a user name'});
       }else{
@@ -25,12 +24,31 @@ module.exports = (router) =>{
 
           user.save((err)=>{
             if(err){
+
               console.log(err);
               if(err.code === 11000){
                 res.json({success:false, message:'UserName/Email already exists'});
               }else{
-                res.json({success:false, message:'could not save users'});
-              }
+                if(err.errors){
+                  // console.log('inside');
+                  if(err.errors.email){
+                    res.json({success:false, message: err.errors.email.message});
+                  }else{
+                    if(err.errors.userName){
+                      res.json({success:false, message: err.errors.userName.message});
+                    }else{
+                      if(err.errors.password){
+                        res.json({success:false, message: err.errors.password.message});
+                      }else{
+                        res.json({success:false})
+                      }
+                    }
+                  }
+                }else{
+                  res.json({success:false, message:'could not save users', err});
+                  }
+                }
+              // }
             }else{
               res.json({success:true, message:'User registered successfully'});
             }
